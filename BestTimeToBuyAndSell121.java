3237017e1b5d82424e54f3bea6db5b980c9e9bf7
tushar bansod <tbansod2018@gmail.com>
2023-01-24 23:01:48 +0530

    file modified

--- BestTimeToBuyAndSell121.java
+++ BestTimeToBuyAndSell121.java
@@ -1,8 +1,6 @@
 package Leetcode;
 
-import java.util.ArrayList;
-
-public class BestTimeToButAndSell121 {
+public class BestTimeToBuyAndSell121 {
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices));
